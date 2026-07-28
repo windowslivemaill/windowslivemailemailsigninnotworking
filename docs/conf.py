e241444b9ca258880afa80f4ecf079f619eeffec
windowslivemaill <majje38952@outlook.com>
2026-07-28 05:49:23 -0700

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+project = 'Windows Live Mail Email Sign In Not Working'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+html_js_files = [
+    'chatbot.js',
+]
+html_favicon = '.png'
